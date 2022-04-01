c37bac6922d0020cff11b059fd0aee4a20fc4d10
Shusaku Yusa <shu.yusa@gmail.com>
2022-04-01 17:46:54 +0900

    Use configuration file

--- src/main/java/ApiServer.java
+++ src/main/java/ApiServer.java
@@ -4,23 +4,33 @@ import adapter.OneFrameApi;
 import com.sun.net.httpserver.HttpServer;
 import handler.HttpHandler;
 
+import java.io.InputStream;
 import java.util.concurrent.Executors;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.Properties;
 
 public class ApiServer {
     public static void main(String... args) throws IOException {
-        HttpHandler handler = new HttpHandler(
-            new ExchangeRateService(
-                new OneFrameApi(
-                    new ApiConfig(
-                        "http://localhost:8080", "10dc303535874aeccc86a8251e6992f5"))));
+        int serverPort;
+        ApiConfig apiConfig;
+        try (InputStream input = ApiServer.class.getClassLoader().getResourceAsStream("config.properties")) {
+            Properties prop = new Properties();
+            prop.load(input);
+            serverPort = Integer.parseInt(prop.getProperty("server.port"));
+            apiConfig = new ApiConfig(prop.getProperty("service.host"), prop.getProperty("service.token"));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return;
+        }
 
-        HttpServer server = HttpServer.create(new InetSocketAddress(8888), 0);
+        HttpHandler handler = new HttpHandler(new ExchangeRateService(new OneFrameApi(apiConfig)));
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(serverPort), 0);
         server.setExecutor(Executors.newCachedThreadPool());
         server.createContext("/", handler::handle);
         server.start();
-        System.out.println("start listening on port 80");
+        System.out.printf("start listening on port %s%n", serverPort);
     }
 }
--- src/test/java/ApiServerTest.java
+++ src/test/java/ApiServerTest.java
@@ -2,17 +2,25 @@ import org.json.JSONObject;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Properties;
 
 import static org.junit.Assert.*;
 
 public class ApiServerTest {
     @Test
     public void testServer() throws InterruptedException, IOException {
-        URI url = URI.create("http://127.0.0.1:8888?from=USD&to=JPY");
+        int serverPort;
+        InputStream input = ApiServer.class.getClassLoader().getResourceAsStream("config.properties");
+        Properties prop = new Properties();
+        prop.load(input);
+        serverPort = Integer.parseInt(prop.getProperty("server.port"));
+
+        URI url = URI.create(String.format("http://127.0.0.1:%s?from=USD&to=JPY", serverPort));
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(url)
                 .header("Accept", "application/json")
